0439cc54de6e0c270019b810838358dc62b244ca
Neil Mayhew <neil@webuildlegends.com>
2018-05-13 17:26:19 -0600

    Use a standard nix-file configuration

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-  let
-    inherit (nixpkgs) pkgs;
+{ stdenv, mkDerivation, base, bytestring, utf8-string, cmdargs, gio, gtk
+}:
 
-    haskellPackages = if compiler == "default"
-                         then pkgs.haskellPackages
-                         else pkgs.haskell.packages.${compiler};
-  in
-    haskellPackages.callPackage ./emblems.nix { }
+mkDerivation {
+  pname = "emblems";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [ base bytestring utf8-string cmdargs gio gtk ];
+  description = "A command-line utility for manipulating emblems on file icons";
+  license = stdenv.lib.licenses.gpl3;
+}
--- shell.nix
+++ shell.nix
@@ -1,2 +1,15 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage ./default.nix {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
